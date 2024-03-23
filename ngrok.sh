0b6c7dd4a0c932b2d6467e5595d7186db7095b0d
n10XBi <164579514+n10XBi@users.noreply.github.com>
2024-03-23 19:38:28 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -5,17 +5,15 @@ stty susp undef
 
 clear
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
-echo "======================================="
-echo "         Selamat Datang di GenDev      "
-echo "        (Keren & Kreatif Ubuntu)       "
-echo "======================================="
+echo "======================="
 echo "Mengunduh ngrok..."
 echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip > /dev/null 2>&1
-unzip ngrok.zip > /dev/null 2>&1
+echo "Translate ByGenDev Fork Repo: 💥https://github.com/n10XBi/Docker-Ubuntu-Desktop-NoMachine💥"
 echo "===================================="
 echo "Selamat datang di Skrip Ubuntu Gendev"
 echo "===================================="
 echo "Skrip ini digunakan untuk ..."
 echo "Proses unduhan dan konfigurasi ngrok telah selesai."
 echo "Terima kasih telah menggunakan skrip ini."
+wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
+tar -xvzf ngrok.tgz > /dev/null 2>&1
