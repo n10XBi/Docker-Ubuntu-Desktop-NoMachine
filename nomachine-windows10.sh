19f6db00f5bf593e02977f1a1b4ddbba3d6cc5b3
n10XBi <164579514+n10XBi@users.noreply.github.com>
2024-03-23 15:50:13 +0700

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -2,7 +2,6 @@ wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/ma
 chmod +x ng.sh
 ./ng.sh
 
-
 function goto
 {
     label=$1
@@ -15,32 +14,32 @@ function goto
 
 : ngrok
 clear
-echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
-read -p "Paste Ngrok Authtoken: " CRP
+echo "Buka: https://dashboard.ngrok.com/get-started/your-authtoken"
+read -p "Tempelkan Authtoken Ngrok: " CRP
 ./ngrok config add-authtoken $CRP 
 clear
 echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
 echo "======================="
-echo "choose ngrok region (for better connection)."
+echo "Pilih wilayah Ngrok (untuk koneksi yang lebih baik)."
 echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
+echo "us - Amerika Serikat (Ohio)"
+echo "eu - Eropa (Frankfurt)"
+echo "ap - Asia/Pasifik (Singapura)"
 echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
+echo "sa - Amerika Selatan (Sao Paulo)"
+echo "jp - Jepang (Tokyo)"
 echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
+read -p "Pilih wilayah Ngrok: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
 sleep 1
-if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Kesalahan Ngrok! Silakan coba lagi!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
 clear
 echo "NoMachine: https://www.nomachine.com/download"
-echo Done! NoMachine Information:
-echo IP Address:
+echo Selesai! Informasi NoMachine:
+echo Alamat IP:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
-echo User: user
-echo Passwd: 123456
-echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
+echo Pengguna: user
+echo Kata sandi: 123456
+echo "VM tidak dapat terhubung? Mulai ulang Cloud Shell lalu jalankan ulang skrip."
+seq 1 43200 | while read i; do echo -en "\r Menjalankan .     $i detik / 43200 detik";sleep 0.1;echo -en "\r Menjalankan ..    $i detik / 43200 detik";sleep 0.1;echo -en "\r Menjalankan ...   $i detik / 43200 detik";sleep 0.1;echo -en "\r Menjalankan ....  $i detik / 43200 detik";sleep 0.1;echo -en "\r Menjalankan ..... $i detik / 43200 detik";sleep 0.1;echo -en "\r Menjalankan     . $i detik / 43200 detik";sleep 0.1;echo -en "\r Menjalankan  .... $i detik / 43200 detik";sleep 0.1;echo -en "\r Menjalankan   ... $i detik / 43200 detik";sleep 0.1;echo -en "\r Menjalankan    .. $i detik / 43200 detik";sleep 0.1;echo -en "\r Menjalankan     . $i detik / 43200 detik";sleep 0.1; done
