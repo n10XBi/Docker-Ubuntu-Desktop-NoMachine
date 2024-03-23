803786af9c2eba78cb3111b16c3ec4b7b5b767a5
n10XBi <164579514+n10XBi@users.noreply.github.com>
2024-03-23 16:19:47 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -6,8 +6,13 @@ stty susp undef
 clear
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
-echo "Downloading ngrok..."
+echo "Mengunduh ngrok..."
 echo "======================="
 wget -O ngrok.tgz https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 tar -xvzf ngrok.tgz > /dev/null 2>&1
-
+echo "===================================="
+echo "Selamat datang di Skrip Ubuntu Gendev"
+echo "===================================="
+echo "Skrip ini digunakan untuk ..."
+echo "Proses unduhan dan konfigurasi ngrok telah selesai."
+echo "Terima kasih telah menggunakan skrip ini."
